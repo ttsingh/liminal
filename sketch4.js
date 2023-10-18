fae397766847d8b55c74f766d48ca3376337ff1b
ttsingh <125217412+ttsingh@users.noreply.github.com>
2023-10-18 13:01:24 -0700

    5

---
+++ sketch4.js
@@ -0,0 +1,36 @@
+var foundTheSquare = false;
+var imgSquare;
+var imgCircle;
+
+function preload() {
+  imgSquare = loadImage('img/cat.png');
+  imgCircle = loadImage('img/stand.png');
+}
+
+function setup() {
+  createCanvas(1440, 500);
+  //randomize the square location
+  squareX = random(width - imgSquare.width / 2);
+  squareY = random(height - imgSquare.height / 2);
+  rectMode(CENTER);
+}
+
+function draw() {
+  if (!foundTheSquare) {
+    background(0);
+    image(imgCircle, mouseX - imgCircle.width / 2, mouseY - imgCircle.height / 2);
+    image(imgSquare, squareX - imgSquare.width / 2, squareY - imgSquare.height / 2);
+  } else {
+    background(255, 255, 255);
+    textSize(32);
+    textAlign(CENTER);
+    text("you found the cat", width / 2, height / 2);
+  }
+}
+
+function mouseClicked() {
+  if (mouseX > squareX - imgSquare.width / 2 && mouseX < squareX + imgSquare.width / 2 &&
+    mouseY > squareY - imgSquare.height / 2 && mouseY < squareY + imgSquare.height / 2) {
+    foundTheSquare = true;
+  }
+}
